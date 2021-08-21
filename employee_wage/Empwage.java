777282a2d24daba920b845ad5daa12269a91d3ae
Adarsh999bh <adarshbhandary48@gmail.com>
2021-08-21 20:34:57 +0530

    skeleton of empwage created

---
+++ employee_wage/Empwage.java
@@ -0,0 +1,20 @@
+package employee_wage;
+class Company{
+    public static String compName;
+    
+}
+class Employee extends Company{
+
+    Employee(){
+    }
+}
+class Wage extends Employee{
+    Wage(){
+
+    }
+}
+public class Empwage{
+    public static void main(String[] args) {
+        
+    }
+}
