f4fe3ba199841d5bb03da31cdcecab733a0e175f
Adarsh999bh <adarshbhandary48@gmail.com>
2021-08-22 18:19:58 +0530

    functionalities added working fine

--- employee_wage/Empwage.java
+++ employee_wage/Empwage.java
@@ -5,8 +5,12 @@ class Company{
     //inner class Employee
     class Employee{
 
-        // Employee(){
-        // }
+        Employee(){
+            employee_working_status_for_the_day=0;
+            monthly_overtime=0;
+            total_hours_worked_in_month=0;
+            total_wage_per_month=0;
+        }
         public int employee_working_status_for_the_day;//absent 0 part-time 1 full-time 2
         public int monthly_overtime;
         public int total_hours_worked_in_month;
@@ -50,18 +54,60 @@ class Company{
         this.no_of_emp=no_of_emp;
         this.no_working_days_per_month=no_working_days_per_month;
         this.ep=new Employee[this.no_of_emp];
+        for(int i=0;i<this.no_of_emp;i++){
+            this.ep[i]=new Employee();
+        }
+        this.cost_for_comp_full_emp=0;
+        this.cost_for_comp_part_emp=0;
     }
 
     //company details printing
     public void displayCompDetails(){
-        System.out.println("Company name : "+compName);
-        System.out.println("Max working hours per month : "+max_working_hours_per_month);
-        System.out.println("Part-time hour : "+part_time_hour);
-        System.out.println("Full-time hour : "+full_time_hour);
-        System.out.println("Employye wage per hour : "+employee_wage_per_hour);
-        System.out.println("Number of working days in a month : "+no_working_days_per_month);
-        System.out.println("total cost for company on part-time employee is "+cost_for_comp_part_emp);
-        System.out.println("total cost for company on full-time employee is "+cost_for_comp_full_emp);
+        System.out.println("Company name : "+this.compName);
+        System.out.println("Max working hours per month : "+this.max_working_hours_per_month);
+        System.out.println("Part-time hour : "+this.part_time_hour);
+        System.out.println("Full-time hour : "+this.full_time_hour);
+        System.out.println("Employye wage per hour : "+this.employee_wage_per_hour);
+        System.out.println("Number of working days in a month : "+this.no_working_days_per_month);
+        System.out.println("total cost for company on part-time employee is "+this.cost_for_comp_part_emp);
+        System.out.println("total cost for company on full-time employee is "+this.cost_for_comp_full_emp);
+    }
+    public void calculate(){
+        for(int j=0;j<this.no_working_days_per_month;j++){
+            for (int i=0;i<this.no_of_emp;i++){
+                int status=(int) (Math.floor(Math.random()*10)%3);
+                if(status==0){
+                    //System.out.println("Employee "+(i+1)+" is Absent on day "+j);
+                    this.ep[i].setStatus(status);
+                    this.ep[i].printStatus(i+1, j+1);
+                }
+                else if (status==1){
+                    // System.out.println("Employee "+(i+1)+" is Present (Part-time) on day "+j);
+                    // pt_wage+=80;
+                    // ep_wage[i]+=80;
+                    this.ep[i].setStatus(status);
+                    this.ep[i].printStatus(i+1, j+1);
+                    this.ep[i].total_hours_worked_in_month+=this.part_time_hour;
+                    this.ep[i].total_wage_per_month+=this.part_time_hour * this.employee_wage_per_hour;
+                    this.cost_for_comp_part_emp+=this.part_time_hour * this.employee_wage_per_hour;
+                }
+                else{
+                    // System.out.println("Employee "+(i+1)+" is Present (Full-time) on day "+j);
+                    // ft_wage+=160;
+                    // ep_wage[i]=160;
+                    this.ep[i].setStatus(status);
+                    this.ep[i].printStatus(i+1, j+1);
+                    this.ep[i].total_hours_worked_in_month+=this.full_time_hour;
+                    this.ep[i].total_wage_per_month+=this.full_time_hour * this.employee_wage_per_hour;
+                    this.cost_for_comp_full_emp+=this.full_time_hour * this.employee_wage_per_hour;
+                }
+            }
+        }
+        for(int i=0;i<this.no_of_emp;i++){
+            System.out.println("Employee "+(i+1));
+            this.ep[i].employeeDetails();
+        }
+        displayCompDetails();
     }
 }
 
@@ -77,7 +123,6 @@ public class Empwage{
         for(int i=0;i<n;i++){
             System.out.print("Enter the company name : ");
             String compname=sc_string.nextLine();
-            sc_string.close();
             System.out.print("Enter the number of employees : ");
             int no_e=sc_int.nextInt();
             System.out.print("Enter max working hours per month : ");
@@ -91,6 +136,7 @@ public class Empwage{
             System.out.print("Enter the number of working days in month : ");
             int wds=sc_int.nextInt();
             cp[i]=new Company(compname, mwh, pt, ft, wph, wds, no_e);
+            cp[i].calculate();
         }
         sc_int.close();
         sc_string.close();
